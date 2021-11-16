24ce07e608533d9f7cc46bf92fe46980487d2c12
Daniel Chaves <danielchaves001@gmail.com>
2021-11-15 21:31:02 -0300

    Reoaganizada a estrutura de visualização e adiocionado o Vigesilhão

--- numero_por_extenso/numeros_base.py
+++ numero_por_extenso/numeros_base.py
@@ -1,19 +1,121 @@
 DECIMAIS = (
-    ('décimo', 'décimos'), ('centésimo', 'centésimos'), ('milésimo', 'milésimos'), ('décimo de milésimo', 'décimos de milésimo'),
-    ('centésimo de milésimo', 'centésimos de milésimo'), ('milionésimo', 'milionésimos'), ('décimo de milionésimo', 'décimos de milionésimo'),
-    ('centésimo de milionésimo', 'centésimos de milionésimo'), ('bilionésimo', 'bilionésimos'), ('décimo de bilionésimo', 'décimos de bilionésimo'), 
-    ('centésimo de bilionésimo', 'centésimos de bilionésimo'), ('trilionésimo', 'trilionésimos'), ('quatrilionésimo', 'quatrilionésimos'),
-    ('quintilionésimo', 'quintilionésimos'), ('sextilionésimo', 'sextilionésimos'), ('septilionésimo', 'septilionésimos'), ('octilionésimo', 'octilionésimos'),
-    ('nonilionésimo', 'nonilionésimos'), ('decilionésimo', 'decilionésimos')
+    ('décimo', 'décimos'), 
+    ('centésimo', 'centésimos'), 
+
+    ('milésimo', 'milésimos'), 
+    ('décimo de milésimo', 'décimos de milésimo'),
+    ('centésimo de milésimo', 'centésimos de milésimo'), 
+    
+    ('milionésimo', 'milionésimos'), 
+    ('décimo de milionésimo', 'décimos de milionésimo'),
+    ('centésimo de milionésimo', 'centésimos de milionésimo'), 
+    
+    ('bilionésimo', 'bilionésimos'), 
+    ('décimo de bilionésimo', 'décimos de bilionésimo'), 
+    ('centésimo de bilionésimo', 'centésimos de bilionésimo'), 
+    
+    ('trilionésimo', 'trilionésimos'), 
+    ('décimo de trilionésimo', 'décimos de trilionésimo'), 
+    ('centésimo de trilionésimo', 'centésimos de trilionésimo'), 
+    
+    ('quatrilionésimo', 'quatrilionésimos'),
+    ('décimo de quatrilionésimo', 'décimos de quatrilionésimo'), 
+    ('centésimo de quatrilionésimo', 'centésimos de quatrilionésimo'), 
+    
+    ('quintilionésimo', 'quintilionésimos'), 
+    ('décimo de quintilionésimo', 'décimos de quintilionésimo'), 
+    ('centésimo de quintilionésimo', 'centésimos de quintilionésimo'), 
+    
+    ('sextilionésimo', 'sextilionésimos'), 
+    ('décimo de sextilionésimo', 'décimos de sextilionésimo'), 
+    ('centésimo de sextilionésimo', 'centésimos de sextilionésimo'), 
+    
+    ('septilionésimo', 'septilionésimos'), 
+    ('décimo de septilionésimo', 'décimos de septilionésimo'), 
+    ('centésimo de septilionésimo', 'centésimos de septilionésimo'), 
+    
+    ('octilionésimo', 'octilionésimos'),
+    ('décimo de octilionésimo', 'décimos de octilionésimo'),
+    ('centésimo de octilionésimo', 'centésimos de octilionésimo'),
+    
+    ('nonilionésimo', 'nonilionésimos'), 
+    ('décimo de nonilionésimo', 'décimos de nonilionésimo'), 
+    ('centésimo de nonilionésimo', 'centésimos de nonilionésimo'), 
+    
+    ('decilionésimo', 'decilionésimos'),
+    ('décimo de decilionésimo', 'décimos de decilionésimo'),
+    ('centésimo de decilionésimo', 'centésimos de decilionésimo'),
 )
-UNIDADES = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove')
-DEZENA_ESPECIAL = ('', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove')
-DEZENAS = ('', 'dez', 'vinte', 'trinta', 'quarenta', 'cinquenta', 'sessenta', 'setenta', 'oitenta', 'noventa')
-CENTENAS = ('cem', 'cento', 'duzentos', 'trezentos', 'quatrocentos', 'quinhentos', 'seiscentos', 'setecentos','oitocentos', 'novecentos')
+UNIDADES = (
+    'zero', 
+    'um', 
+    'dois', 
+    'três', 
+    'quatro', 
+    'cinco', 
+    'seis', 
+    'sete', 
+    'oito', 
+    'nove')
+    
+DEZENA_ESPECIAL = (
+    '', 
+    'onze', 
+    'doze', 
+    'treze', 
+    'quatorze', 
+    'quinze', 
+    'dezesseis', 
+    'dezessete', 
+    'dezoito', 
+    'dezenove'
+)
+
+DEZENAS = (
+    '', 
+    'dez', 
+    'vinte', 
+    'trinta', 
+    'quarenta', 
+    'cinquenta', 
+    'sessenta', 
+    'setenta', 
+    'oitenta', 
+    'noventa'
+)
+
+CENTENAS = (
+    'cem', 
+    'cento', 
+    'duzentos', 
+    'trezentos', 
+    'quatrocentos', 
+    'quinhentos', 
+    'seiscentos', 
+    'setecentos',
+    'oitocentos', 
+    'novecentos'
+)
+
 MILHAR = (
-    ('milhão', 'milhões'), ('bilhão', 'bilhões'), ('trilhão', 'trilhões'), ('quatrilhão', 'quatrilhões'), 
-    ('quintilhão', 'quintilhões'), ('sextilhão', 'sextilhões'), ('septilhão', 'septilhões'), ('octilhão', 'octilhões'), ('nonilhão', 'nonilhões'), ('decilhão', 'decilhões'),
-    ('unodecilhão', 'unodecilhões'), ('duodecilhão', 'duodecilhões'), ('tredecilhão', 'tredecilhões'), ('quatuordecilhão', 'quatuordecilhões'),
-    ('quindecilhão', 'quindecilhões'), ('sexdecilhão', 'sexdecilhões'), ('sepdecilhão', 'sepdecilhões'), ('octodecilhão', 'octodecilhões'),
-    ('novemdecilhão', 'novemdecilhões')
+    ('milhão', 'milhões'), 
+    ('bilhão', 'bilhões'), 
+    ('trilhão', 'trilhões'), 
+    ('quatrilhão', 'quatrilhões'), 
+    ('quintilhão', 'quintilhões'), 
+    ('sextilhão', 'sextilhões'), 
+    ('septilhão', 'septilhões'), 
+    ('octilhão', 'octilhões'), 
+    ('nonilhão', 'nonilhões'), 
+    ('decilhão', 'decilhões'),
+    ('unodecilhão', 'unodecilhões'), 
+    ('duodecilhão', 'duodecilhões'), 
+    ('tredecilhão', 'tredecilhões'), 
+    ('quatuordecilhão', 'quatuordecilhões'),
+    ('quindecilhão', 'quindecilhões'), 
+    ('sexdecilhão', 'sexdecilhões'), 
+    ('sepdecilhão', 'sepdecilhões'), 
+    ('octodecilhão', 'octodecilhões'),
+    ('novemdecilhão', 'novemdecilhões'),
+    ('Vigesilhão', 'Vigesilhões '),
 )
