8ad75faf3c7e35486a7dcbf65ea2e43068f2c156
Daniel Chaves <danielchaves001@gmail.com>
2021-11-15 21:30:14 -0300

    Refatorado os teste unutários do módulo 'real'

--- test/numero_por_extenso_teste.py
+++ test/numero_por_extenso_teste.py
@@ -4,20 +4,25 @@ sys.path.insert(0, './numero_por_extenso')
 
 import numero_por_extenso
 
-from testes import formatar_numero_testes, real_testes, monetario_testes
+from testes import formatar_numero_testes, real_fracoes_testes, real_testes, monetario_testes
 
 
 class TestNumeroPorExtenso(unittest.TestCase):
 
     def test_formatar_numero(self):
-        for saida, numeros in formatar_numero_testes:
+        for saida, padrao, numeros in formatar_numero_testes:
             for entrada in numeros:
-                self.assertEqual(numero_por_extenso.formatar_numero(entrada), saida)
+                self.assertEqual(numero_por_extenso.formatar_numero(entrada, padrao), saida)
+
+    def test_real_fracoes(self):
+        for saida, padrao, numeros in real_fracoes_testes:
+            for entrada in numeros:
+                self.assertEqual(numero_por_extenso.real(entrada, padrao), saida)
 
     def test_real(self):
-        for teste in real_testes:
-            for numero in teste[1:]:
-                self.assertEqual(numero_por_extenso.real(numero), teste[0])
+        for saida, padrao, numeros in real_testes:
+            for entrada in numeros:
+                self.assertEqual(numero_por_extenso.real(entrada, padrao), saida)
 
     def test_monetario(self):
         for teste in monetario_testes:
